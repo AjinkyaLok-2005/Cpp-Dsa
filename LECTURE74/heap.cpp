a493ff23eb5271d746be262a2cff50e4f451d338
ajinkya947 <lajinkya645@gmail.com>
2025-07-26 23:10:19 +0530

    Heap

--- LECTURE74/heap.cpp
+++ LECTURE74/heap.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<queue>
 using namespace std;
 
 class heap
@@ -80,17 +81,17 @@ class heap
     }
 };
 
-//Heapify for MaxHeap
+//Heapify for MaxHeap 
 void heapify(int arr[], int n, int i)
 {
     int largest = i;
     int left = 2*i;
     int right  = 2*i + 1;
-    if(left < n && arr[largest] < arr[left])
+    if(left <= n && arr[largest] < arr[left])
     {
         largest = left;
     }
-    if(right < n && arr[largest] < arr[right])
+    if(right <= n && arr[largest] < arr[right])
     {
         largest = right;
     }
@@ -98,6 +99,19 @@ void heapify(int arr[], int n, int i)
     {
         swap(arr[largest], arr[i]);
         heapify(arr, n, largest);
+    }
+}
+
+void heapSort(int arr[], int n)
+{
+    int size = n;
+
+    while(size > 1)
+    {
+        swap(arr[size], arr[1]);
+        size--;
+
+        heapify(arr, size, 1);
     }
 }
  
@@ -121,7 +135,7 @@ int main()
     {
         heapify(arr, n, i);
     }
-    
+
     cout << "printing the array now " << endl;
 
     for(int i = 1; i <= n; i++)
@@ -130,6 +144,62 @@ int main()
     }
 
     cout << endl;
+
+    heapSort(arr, n);
+
+    cout << "Printing sorted array" << endl;
+    for(int i = 1; i <= n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    cout << "Using Priority Queue here " << endl;
+    
+    //maxHeap
+    priority_queue<int> pq;
+
+    pq.push(4);
+    pq.push(2);
+    pq.push(5);
+    pq.push(3);
+
+    cout << "element at top " << pq.top() << endl;
+    pq.pop();
+    cout << "element at top " << pq.top() << endl; 
+
+    cout << "Size is " << pq.size() << endl;
+
+    if(pq.empty())
+    {
+        cout << "pq is empty " << endl;
+    }
+    else
+    {
+        cout << "pq is not empty " << endl;
+    }
+
+    priority_queue<int, vector<int>, greater<int>> minheap;
+
+    minheap.push(4);
+    minheap.push(2);
+    minheap.push(5);
+    minheap.push(3);
+
+    cout << "element at top " << minheap.top() << endl;
+    minheap.pop();
+    cout << "element at top " << minheap.top() << endl; 
+
+    cout << "Size is " << minheap.size() << endl;
+
+    if(minheap.empty())
+    {
+        cout << "minheap is empty " << endl;
+    }
+    else
+    {
+        cout << "minheap is not empty " << endl;
+    }
 
     return 0;
 }
