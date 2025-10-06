e7e40e9cd175b39537d512c4c9c3846145f4a5c7
ajinkya947 <lajinkya645@gmail.com>
2025-10-07 00:16:25 +0530

    Fibonacci using DP

--- LECTURE102/FibonacciDP.cpp
+++ LECTURE102/FibonacciDP.cpp
@@ -45,24 +45,48 @@ using namespace std;
 //TABULAR METHOD
 
 int main()
-{   
+// {   
+//     int n;
+//     cin >> n;
+
+//     //step1 
+//     vector<int> dp(n+1);
+
+//     //step2
+//     dp[1] = 1;
+//     dp[0] = 0;
+
+//     //step3
+//     for(int i = 2; i <= n; i++)
+//     {
+//         dp[i] = dp[i-1] + dp[i-2];
+//     }
+
+//     cout << dp[n] << endl;
+
+//     return 0;
+// }
+
+
+//SPACE OPTIMISATION
+
+int main()
+{
     int n;
     cin >> n;
 
-    //step1 
-    vector<int> dp(n+1);
+    int prev1 = 1;
+    int prev2 = 0;
 
-    //step2
-    dp[1] = 1;
-    dp[0] = 0;
-
-    //step3
     for(int i = 2; i <= n; i++)
     {
-        dp[i] = dp[i-1] + dp[i-2];
+        int curr = prev1 + prev2;
+        //shift logic
+        prev2 = prev1;
+        prev1 = curr;
     }
 
-    cout << dp[n] << endl;
+    cout << prev1 << endl;
 
     return 0;
 }
